19bf39fe2de7a1d72ab71464e22a9eef2b75d222
gauravchandrakar25 <gauravchandrakar25@gmail.com>
2024-01-27 20:57:44 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const Hello = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  );
+};
+
+const Footer = () => {
+  return (
+    <div>
+      <p>a footer</p>
+    </div>
+  );
+};
+
+const App = () => {
+  const name = "Peter";
+  const age = 10;
+
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
